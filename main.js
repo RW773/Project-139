b1511f59227fbac519b556e276bcf8ee3c04061f
RW773 <111027857+RW773@users.noreply.github.com>
2023-03-01 18:49:53 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+status = "";
+
+function preload(){
+  img = loadImage('dog_cat.jpg');
+}
+
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+  objectDetector.detect(img, gotResult);
+}
